446a7f1dbe5528c4b42c4bea9bcf275899ca2fd4
Matthaeus <matthaeusbm@gmail.com>
2024-11-10 16:03:17 -0300

    chore: uninstall next auth

--- app/_actions/update-user.tsx
+++ app/_actions/update-user.tsx
@@ -1,28 +1,13 @@
 "use server"
 
-import { db } from "../_lib/prisma"
-import { getServerSession } from "next-auth"
-import { authOptions } from "../_lib/auth"
+// interface UpdateUserParams {
+//   username: string
+//   experienceLevel: string
+//   favoriteSystems: string[]
+//   gameStyle: string[]
+//   safety: string[]
+//   goals: string[]
+//   logistics: string[]
+// }
 
-interface UpdateUserParams {
-  username: string
-  experienceLevel: string
-  favoriteSystems: string[]
-  gameStyle: string[]
-  safety: string[]
-  goals: string[]
-  logistics: string[]
-}
-
-export const updateUser = async (data: UpdateUserParams) => {
-  const session = await getServerSession(authOptions)
-  if (!session?.user) {
-    throw new Error("Usuário não autenticado")
-  }
-  await db.user.update({
-    where: {
-      id: session?.user?.id,
-    },
-    data: data,
-  })
-}
+export const updateUser = async () => {}
